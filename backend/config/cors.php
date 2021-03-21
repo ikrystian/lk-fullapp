2531b135a63da85b3737559786d91f28cdf8d510
Krystian <krystian@bpcoders.pl>
2021-03-21 11:41:40 +0100

    cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,46 +1,13 @@
 <?php
+
+
 return [
-
-    'cors_profile' => Spatie\Cors\CorsProfile\DefaultProfile::class,
-
-    /*
-    * This configuration is used by `DefaultProfile`.
-    */
-    'default_profile' => [
-        'allow_credentials' => false,
-        'allow_origins' => [
-            '*',
-        ],
-        'allow_methods' => [
-            'POST',
-            'GET',
-            'OPTIONS',
-            'PUT',
-            'PATCH',
-            'DELETE',
-        ],
-
-        'allow_headers' => [
-            'Content-Type',
-            'X-Auth-Token',
-            'Origin',
-            'Authorization',
-        ],
-
-        'expose_headers' => [
-            'Cache-Control',
-            'Content-Language',
-            'Content-Type',
-            'Expires',
-            'Last-Modified',
-            'Pragma',
-        ],
-
-        'forbidden_response' => [
-            'message' => 'Forbidden (cors).',
-            'status' => 403,
-        ],
-
-        'max_age' => 60 * 60 * 24,
-    ],
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
