4260f9eb15b0efae7fbf7b07d5c6ae6550413914
Krystian <krystian@bpcoders.pl>
2021-03-27 23:06:41 +0100

    trying fix for bottom nav

--- frontend/src/app/shared/bottom-nav/bottom-nav.component.ts
+++ frontend/src/app/shared/bottom-nav/bottom-nav.component.ts
@@ -15,6 +15,7 @@ import { GeolocationService } from '@ng-web-apis/geolocation';
 import { Location } from '@angular/common';
 import { User } from '../../profile/profile.component';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-bottom-nav',
@@ -29,7 +30,7 @@ export class BottomNavComponent implements OnInit {
   userPosition;
   length;
   training;
-  user: User;
+  user;
   trainings: any;
   USER_IMAGE = 'https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-1/p200x200/105966252_10216830499211784_1193981541556713056_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=7206a8&_nc_ohc=3nQs7TVM02IAX8754nu&_nc_ht=scontent-frt3-1.xx&tp=6&oh=f38da299e4057b6485d123c3645dc1e0&oe=6081A1AE';
 
@@ -41,17 +42,17 @@ export class BottomNavComponent implements OnInit {
     public router: Router,
     private geolocation: GeolocationService,
   ) {
+    this.jwtService.profile().subscribe((res: any) => {
+      this.user = res;
+    });
 
     this.geolocation.subscribe(position => {
       this.userPosition = {latitude: position.coords.latitude, longitude: position.coords.longitude};
     });
-
-    this.jwtService.profile().subscribe((res: any) => {
-      this.user = res;
-    });
   }
 
   ngOnInit(): void {
+
   }
 
   onDarkModeSwitched({checked}: MatSlideToggleChange): void {
