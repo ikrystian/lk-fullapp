bf7efd4eb5d1fd00a44ee439b423fc6f6c9af805
Krystian <krystian@bpcoders.pl>
2022-04-04 23:52:11 +0200

    Remove add ru button

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8000/api',
+  API_URL: 'https://lk.bpc-dev.pl/backend/public/api',
   UPLOADED_ASSETS_URL: 'https://lk.bpc-dev.pl/backend/public/storage/'
 };
